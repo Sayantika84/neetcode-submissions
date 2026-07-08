87532bd856a5189255acf9d762ebaedfe7015f41
Sayantika Mondal <sayantikam769@gmail.com>
2026-07-08 14:59:08 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int n = prices.size();
+        if(n==1) return 0;
+        int ans = 0;
+        for(int i=1; i<n; i++){
+            ans = max(ans, prices[i]-mini);
+            mini = min(mini,prices[i]);
+        }
+        return ans;
+    }
+};
